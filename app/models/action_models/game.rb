bd69189b9c073a191da0202142fe447fdd22015d
Axel Chalon <axel.chalon@hetic.net>
2016-12-10 23:20:27 +0100

    REDIS logique ok mais pas toasté ; Morgan est sur une chaise rouge.

--- app/models/action_models/game.rb
+++ app/models/action_models/game.rb
@@ -1,4 +1,5 @@
-# game_id => {uids: [a,b], words_player0: {duree_que_ca_a_pris_a_trouver_et_a_ecrire: X, mot: Y}, words_player1: {}, next_player:int, last_letter:str,  last_move_timestamp:int}
+# game_id => {uids: [a,b], words_player0: {duree_que_ca_a_pris_a_trouver_et_a_ecrire: X, mot: Y}, words_player1: {}, next_player:int, last_letter:str, score_p0, score_p1 last_move_timestamp:int}
+# score p (incrementent)
 
 class ActionModels::Game
   def self.start(uuid1, uuid2)
@@ -11,6 +12,23 @@ class ActionModels::Game
 
     REDIS.set("opponent_for:#{white}", black)
     REDIS.set("opponent_for:#{black}", white)
+
+
+
+    game_id = SecureRandom.uuid
+
+    REDIS.rpush("game_#{game_id}_pids",white,black)
+
+    # stocker par uid?
+    REDIS.set("game_#{game_id}_p0_score","0")
+    REDIS.set("game_#{game_id}_p1_score","0")
+
+    REDIS.set("game_#{game_id}_next_px","0")
+    REDIS.set("game_#{game_id}_last_letter",('a'..'z').to_a.sample)
+    # REDIS.set("last_move_timestamp","0")
+
+    # REDIS.rpush("game_#{game_id}_words_p0", ..)
+
   end
 
   def self.forfeit(uuid)
@@ -30,9 +48,43 @@ class ActionModels::Game
   #   ActionCable.server.broadcast "player_#{opponent}", {action: "make_move", msg: move_string}
   # end
 
-  def self.play_word(uuid, word)
-    opponent = opponent_for(uuid)
+  def self.play_word(pid, word)
+    opponent = opponent_for(pid)
+
+    pids = REDIS.lrange("game_#{game_id}_pids",0,-1)
+    if (pids[0] == pid)
+      px = "0"
+    elsif (pids[1] == pid)
+      px = "1"
+    else
+      raise new Error("Not in the game")
+    end
+
     # @TODO VERIFY IF WORD IS VALID
+
+    if (REDIS.get("game_#{game_id}_next_px") != px)
+      raise new Error("Not your turn")
+    end
+    REDIS.set("game_#{game_id}_next_px", px == "0" ? "1" : "0")
+
+    if (REDIS.get("game_#{game_id}_last_letter") != word[0])
+      raise new Error("Not your turn")
+    end
+    REDIS.set("game_#{game_id}_last_letter", word[-1])
+
+
+    REDIS.rpush("game_#{game_id}_p#{px}_words",word)
+
+    old_score = REDIS.get("game_#{game_id}_p#{px}_score").to_i
+    old_timestamp = REDIS.get("game_#{game_id}_last_move_timestamp").to_i
+    points = word.length*3 + (Time.now.to_i - old_timestamp)
+    REDIS.set("game_#{game_id}_p#{px}_score",old_score + points)
+
+    if (points > 120)
+      puts "The game is done"
+    end
+
+    REDIS.set("game_#{game_id}_last_move_timestamp", Time.now.to_i)
 
     ActionCable.server.broadcast "player_#{opponent}", {action: "opponent_plays", msg: word}
   end
