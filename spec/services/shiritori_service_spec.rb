3d0ac61a5d008480c12f63f6f39d30647f534c35
Morgan Caron <morgan.caron@hetic.net>
2016-12-18 15:06:47 +0100

    fixed SS test because singleton

--- spec/services/shiritori_service_spec.rb
+++ spec/services/shiritori_service_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe ShiritoriService do
   # todo : comment charger le dictionnaire une seule fois pour tous les tests
   # du service ?
   it "checks french words" do
-    dic = get_shiritori_dic()
-    service = ShiritoriService.new(dic)
-    expect(service.is_this_word_french? 'zubeel').to eq(nil)
-    expect(service.is_this_word_french? 'bonjour').to eq(true)
+    expect(ShiritoriService.instance.is_this_word_french? 'zubeel').to eq(nil)
+    expect(ShiritoriService.instance.is_this_word_french? 'bonjour').to eq(true)
   end
 end
